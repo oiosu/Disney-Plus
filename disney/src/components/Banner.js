0add38f48e2272ac99bbd2838af4b1ba85924ed4
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-12-15 11:25:12 +0900

    show={value.toString()} 오류

--- disney/src/components/Banner.js
+++ disney/src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from '../api/axios';
 import requests from '../api/requests';
+import '../components/Banner.css';
 
 const Banner = () => {
 
@@ -11,22 +12,51 @@ const Banner = () => {
     }, []);
 
     const fetchData = async () => {
-        // 현재 상영중인 영화 정보를 가져오기(여러 영화) 
         const response = await axios.get(requests.fetchNowPlaying);
-        // 여러 영화 중 영화 하나의 ID를 가져오기
         const movieId = response.data.results[
             Math.floor(Math.random() * response.data.results.length)
         ].id;
-        // 특정 영화의 더 상세한 정보를 가져오기(비디오 정보도 포함)
         const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
             params: { append_to_response: "videos" }
         });
 
         setMovie(movieDetail);
     }
+    const truncate = (str, n) => {
+        return str?.length > n ? str.substring(0, n) + '...' : str;
+    }
 
     return (
-        <div>Banner</div>
+        <header
+            className='banner'
+            style={{
+                backgroundImage: movie.backdrop_path
+                    ? `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`
+                    : '',
+                backgroundPosition: "top center",
+                backgroundSize: "cover"
+            }}
+        >
+            <div className='banner__contents'>
+                <h1 className='banner__title'>
+                    {movie.title || movie.name || movie.original_name}
+                </h1>
+
+                <div className='banner__buttons'>
+                    {movie?.videos?.results[0]?.key &&
+                        <button
+                            className='banner__button play'
+                        >
+                            Play
+                        </button>
+                    }
+                </div>
+                <p className='banner__description'>
+                    {truncate(movie.overview, 100)}
+                </p>
+            </div>
+            <div className='banner__fadeBottom' />
+        </header>
     )
 }
 
--- disney/src/components/Nav.js
+++ disney/src/components/Nav.js
@@ -2,24 +2,22 @@ import React, { useState, useEffect, useCallback } from 'react';
 import styled from 'styled-components';
 
 const Nav = () => {
-
     const [show, setShow] = useState(false);
 
-    const handleScroll = () => {
+    const handleScroll = useCallback(() => {
         if (window.scrollY > 50) {
             setShow(true);
         } else {
             setShow(false);
         }
-    }
+    }, []); // 빈 의존성 배열로 변경
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll)
+        window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
-        }
-    }, [handleScroll])
-
+        };
+    }, [handleScroll]);
 
     return (
         <NavWrapper show={show}>
@@ -35,32 +33,32 @@ const Nav = () => {
 };
 
 const NavWrapper = styled.nav`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 70px;
-  background-color: #090b13;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 36px;
-  letter-spacing: 16px;
-  z-index: 3;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 70px;
+    background-color: #090b13;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 36px;
+    letter-spacing: 16px;
+    z-index: 3;
 `;
 
 const Logo = styled.a`
-  padding: 0;
-  width: 80px;
-  margin-top: 4px;
-  max-height: 70px;
-  font-size: 0;
-  display: inline-block;
+    padding: 0;
+    width: 80px;
+    margin-top: 4px;
+    max-height: 70px;
+    font-size: 0;
+    display: inline-block;
 
-  img {
-    display: block;
-    width: 100%;
-  }
+    img {
+        display: block;
+        width: 100%;
+    }
 `;
 
 export default Nav;
